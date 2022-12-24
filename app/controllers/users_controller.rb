e73cb18682ceb8325aa8ddf68f5f1f7120c2327f
Alier Philip <alierditmaguet@gmail.com>
2022-12-24 22:55:05 +0200

    Update User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,48 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find_by(id: params[:id])
+    @posts = @user.most_recent_posts
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit; end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'User was successfully created'
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = 'User was successfully updated'
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:danger] = 'User was successfully deleted'
+    redirect_to users_path
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :photo, :bio, :posts_counter)
   end
 end
