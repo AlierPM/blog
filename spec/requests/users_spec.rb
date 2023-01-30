49da295dae1b0bf627640d02e1f671a1dbdfbd24
Alier  Philip Maguet <alierditmaguet@gmail.com>
2023-01-30 08:27:32 +0200

    Add Rspec test on Users controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,21 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
+RSpec.describe 'UsersControllers', type: :request do
   describe 'GET /index' do
-    before(:example) { get users_path(1) }
-    it 'returns http success' do
-      expect(response).to have_http_status(:success)
-      expect(response).to render_template('index')
-      expect(response.body).to include('Welcome to 1-in-all Blogging site.....')
+    it '/users test' do
+      get '/users'
+      expect(response).to have_http_status(:ok)
+      expect(response).to render_template(:index)
+      expect(response.body).to include 'This is a list of all users'
     end
-  end
 
-  describe 'GET /show' do
-    before(:example) { get user_path(1) }
-    it 'returns http success' do
-      expect(response).to have_http_status(:success)
-      expect(response).to render_template('show')
-      expect(response.body).to include('Meet your finest authors here')
+    it '/users/id test' do
+      get '/users/:id'
+      expect(response).to have_http_status(:ok)
+      expect(response).to render_template(:show)
+      expect(response.body).to include 'This is a single user'
     end
   end
 end
