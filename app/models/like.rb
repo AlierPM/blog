2cb852649b85d6082e0fad1910b381102d589369
Alier  Philip Maguet <alierditmaguet@gmail.com>
2023-02-04 11:20:39 +0200

    Update models for adding form

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,15 +1,9 @@
 class Like < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
-  validates :author_id, presence: true
-  validates :post_id, presence: true
-  validates :author_id, uniqueness: { scope: :post_id }
-
-  after_save :update_like_counter
-
-  private
 
   def update_like_counter
-    post.increment!(:likes_counter)
+    post.likes_counter += 1
+    post.save
   end
 end
