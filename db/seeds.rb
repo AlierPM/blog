0d870c169d1045cd0260b0add97d8c16ada201fa
Alier  Philip Maguet <alierditmaguet@gmail.com>
2023-02-04 11:22:44 +0200

    Configure routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+ 2.times do
+	 User.create(name: Faker::Name.name, photo: Faker::Avatar.image, bio: Faker::Lorem.paragraph, posts_counter: 0)
+ end
+
+#Create at least 4 posts for each user by running the following code in the seed file:
+
+ 4.times do
+	 Post.create(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph, user_id: User.all.sample.id, comments_counter: 0, likes_counter: 0)
+ end
+
+#Create at least 2 comments for each post by running the following code in the seed file:
+
+ 2.times do
+	 Comment.create(content: Faker::Lorem.paragraph, user_id: User.all.sample.id, post_id: Post.all.sample.id)
+ end
+
+#Create at least 2 likes for each post by running the following code in the seed file:
+
+ 2.times do
+	 Like.create(user_id: User.all.sample.id, post_id: Post.all.sample.id)
+ end
