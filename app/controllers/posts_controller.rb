cbe46c073d2c4b96ca5c2ae6140036919eaeac51
Alier  Philip Maguet <alierditmaguet@gmail.com>
2023-02-04 11:19:28 +0200

    Update controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -85,7 +85,7 @@ class PostsController < ApplicationController
   # Never trust parameters from the scary internet, only allow the white list through.
   def post_params
     # params.require(:new_post).permit(:title, :content, :user_id)
-    params.require(:new_post).permit(:title, :text)
+    params.require(:new_post).permit(:title, :content)
   end
 
   def require_same_user
