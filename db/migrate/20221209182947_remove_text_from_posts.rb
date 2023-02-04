6ffa60c645efbeea7b02ad51c00b8287e1cbf0d8
Alier  Philip Maguet <alierditmaguet@gmail.com>
2023-02-04 11:23:24 +0200

    Update database

--- db/migrate/20221209182947_remove_text_from_posts.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTextFromPosts < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :posts, :text, :string
-  end
-end
--- db/migrate/20221210205503_create_posts.rb
+++ db/migrate/20221210205503_create_posts.rb
@@ -2,9 +2,10 @@ class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
       t.string :title
-      t.string :text
+      t.text :content
       t.integer :comments_counter
       t.integer :likes_counter
+      t.references :user, null: false, foreign_key: true, index: true
 
       t.timestamps
     end
